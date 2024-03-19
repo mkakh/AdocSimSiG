c60c8de5701095075087c3d04a7ae8a19603deef
mkakh <akhs3214@gmail.com>
2024-03-19 16:16:19 +0900

    feat(index.html): add a feature to show the directory structure

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use scraper::{Html, Selector};
 use std::{
     env,
-    fs::{create_dir, read_to_string, remove_dir_all, File},
+    fs::{copy, create_dir, read_to_string, remove_dir_all, File},
     io::Write,
     path::{Path, PathBuf},
     process::Command,
 };
 use walkdir::WalkDir;
 
-fn generate_html(source_dir_name: &String) -> Result<(), std::io::Error> {
+fn generate_html(source_dir_name: &str) -> Result<(), std::io::Error> {
+    // remove the trailing slash
+    let source_dir_name = source_dir_name.trim_end_matches('/');
+
     let source_dir = env::current_dir()?.join(source_dir_name);
     let build_dir_prefix = "build";
 
@@ -29,6 +32,7 @@ fn generate_html(source_dir_name: &String) -> Result<(), std::io::Error> {
     }
 
     let mut html_list = vec![];
+    let mut index_adoc = vec!["= Index".to_string(), "".to_string()];
 
     for entry in WalkDir::new(source_dir.clone())
         .follow_links(true)
@@ -57,16 +61,65 @@ fn generate_html(source_dir_name: &String) -> Result<(), std::io::Error> {
             };
 
             if relative_path.is_dir() {
+                let level = relative_path.components().count() - 1;
+                let stars = "*".repeat(level);
+                index_adoc.push(format!(
+                    "{} {}",
+                    stars,
+                    relative_path
+                        .strip_prefix(source_dir_name)
+                        .unwrap()
+                        .display()
+                ));
+
                 create_dir(&target_path)?;
             } else if relative_path.extension() == Some("adoc".as_ref()) {
                 let html_src = run_asciidoctor(None, relative_path)?;
-                let mut f = File::create(target_path)?;
+                let mut f = File::create(&target_path)?;
                 f.write_all(html_src.as_bytes())?;
+                let title = extract_html_title_from_file(&html_src)?;
+
+                let level = relative_path.components().count() - 1;
+                let stars = "*".repeat(level);
+                index_adoc.push(format!(
+                    "{} xref:{}[{}]",
+                    stars,
+                    &target_path
+                        .strip_prefix(build_dir_prefix)
+                        .unwrap()
+                        .to_string_lossy(),
+                    title
+                ));
+            } else {
+                copy(relative_path, target_path)?;
             }
         }
     }
+    // create index.adoc
+    let index_adoc_path = Path::new("build/index.adoc");
+    let mut f = File::create(index_adoc_path)?;
+    writeln!(f, "{}", index_adoc.join("\n"))?;
     generate_index_html(html_list)?;
     Ok(())
+}
+
+fn extract_html_title_from_file(html_file_content: &String) -> Result<String, std::io::Error> {
+    let document = Html::parse_document(&html_file_content);
+
+    // Create a selector for the <title> element
+    let title_selector = Selector::parse("title").unwrap();
+
+    // Find the first matching <title> element
+    if let Some(title_element) = document.select(&title_selector).next() {
+        // Get the text content of the <title> element
+        let title = title_element.text().collect::<String>();
+        Ok(title)
+    } else {
+        Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            "No title is found in the HTML file.",
+        ))
+    }
 }
 
 fn extract_html_title(html_file: &Path) -> Result<String, std::io::Error> {
@@ -94,22 +147,22 @@ fn extract_html_title(html_file: &Path) -> Result<String, std::io::Error> {
 
 fn generate_index_html(html_list: Vec<String>) -> Result<(), std::io::Error> {
     // create index.adoc
-    let index_adoc = Path::new("build/index.adoc");
-    let mut f = File::create(index_adoc)?;
-    writeln!(f, "= Index")?;
-    writeln!(f)?;
-    for path in html_list.iter() {
-        let title = extract_html_title(Path::new(&format!("build/{}", path)))?;
-        writeln!(f, "* xref:{}[{}]", path, title)?;
-    }
+    let index_adoc_path = Path::new("build/index.adoc");
+    //let mut f = File::create(index_adoc)?;
+    //writeln!(f, "= Index")?;
+    //writeln!(f)?;
+    //for path in html_list.iter() {
+    //    let title = extract_html_title(Path::new(&format!("build/{}", path)))?;
+    //    writeln!(f, "* xref:{}[{}]", path, title)?;
+    //}
 
     // generate index.html
-    let html_src = run_asciidoctor(None, index_adoc)?;
+    let html_src = run_asciidoctor(None, index_adoc_path)?;
     let mut f = File::create("build/index.html")?;
     f.write_all(html_src.as_bytes())?;
 
     // remove index.adoc
-    std::fs::remove_file(index_adoc)?;
+    std::fs::remove_file(index_adoc_path)?;
     Ok(())
 }
 
